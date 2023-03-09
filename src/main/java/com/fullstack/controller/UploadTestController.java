81ba1ff8f226a58d944f82168c42e10a43371922
yuseob <dladbtjq@naver.com>
2023-03-09 14:48:33 +0900

    Initial commit

---
+++ src/main/java/com/fullstack/controller/UploadTestController.java
@@ -0,0 +1,13 @@
+package com.fullstack.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UploadTestController {
+
+    @GetMapping("/uploadEx")
+    public void uploadEx() {
+        //View Resolver
+    }
+}
